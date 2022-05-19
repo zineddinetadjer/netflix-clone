67772bca431014fb8b7a2f3412ff2c4831b371a7
zineddinetadjer <zinedinetadjer1@gmail.com>
2022-05-20 00:33:29 +0100

    some modifications on the row.js file

--- zwinflix/src/Row.js
+++ zwinflix/src/Row.js
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./style/row.css";
-import Youtube from "react-youtube";
-import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -18,24 +15,6 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
-  const opts = {
-    height: "390",
-    width: "100%",
-    playerVars: {
-      autoplay: 1,
-    },
-  };
-  const handleClick = (movie) => {
-    if (trailerUrl) {
-      setTrailerUrl("");
-    } else {
-      movieTrailer(movie?.name || "").then((url) => {
-        const urlParams = new URLSearchParams(new URL(url).search);
-        setTrailerUrl(urlParams.get("v"));
-      });
-    }
-  };
-
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -43,7 +22,6 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            onClick={() => handleClick(movie)}
             className={`row-poster ${isLargeRow && "row-posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -52,7 +30,6 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
-      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
